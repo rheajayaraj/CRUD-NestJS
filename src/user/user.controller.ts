52030bd2e53ff15c1dba0280e4e11c0b12bfd569
rheajayarajIO <rhea.jayaraj@bca.christuniversity.in>
2025-05-09 11:09:50 +0530

    CRUD using NestJS

---
+++ src/user/user.controller.ts
@@ -0,0 +1,41 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  Put,
+  Delete,
+} from '@nestjs/common';
+import { UserService } from './user.service';
+import { User } from './user.schema';
+
+@Controller('users')
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Post()
+  create(@Body() user: User) {
+    return this.userService.create(user);
+  }
+
+  @Get()
+  findAll() {
+    return this.userService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.userService.findOne(id);
+  }
+
+  @Put(':id')
+  update(@Param('id') id: string, @Body() user: Partial<User>) {
+    return this.userService.update(id, user);
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: string) {
+    return this.userService.delete(id);
+  }
+}
---
+++ src/user/user.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { User, UserDocument } from './user.schema';
+
+@Injectable()
+export class UserService {
+  constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
+
+  create(user: User): Promise<User> {
+    const newUser = new this.userModel(user);
+    return newUser.save();
+  }
+
+  findAll(): Promise<User[]> {
+    return this.userModel.find().exec();
+  }
+
+  findOne(id: string): Promise<User | null> {
+    return this.userModel.findById(id).exec();
+  }
+
+  update(id: string, user: Partial<User>): Promise<User | null> {
+    return this.userModel.findByIdAndUpdate(id, user, { new: true }).exec();
+  }
+
+  delete(id: string): Promise<User | null> {
+    return this.userModel.findByIdAndDelete(id).exec();
+  }
+}
